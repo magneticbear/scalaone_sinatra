67ce3d5073ad960b7e4c0b3815a884cd68909fe7
Jean-Pierre Simard <jp@jpsim.com>
2012-08-23 15:37:45 -0400

    Now has pusher gem integration and relays the message back on POST

--- scalaone.rb
+++ scalaone.rb
@@ -1,11 +1,25 @@
 require 'sinatra'
 require 'json'
+require 'pusher'
 
-get '/' do
-  "ScalaOne Sinatra app"
-end
+class SinatraPusher < Sinatra::Base
+  configure do
+    register Sinatra::Reloader
+    Pusher.app_id = '26274'
+    Pusher.key = '28f1d32eb7a1f83880af'
+    Pusher.secret = '9514f2b210d56f1d5869'
+  end
 
-get '/messages.json' do
-  content_type :json
-  { :message => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', :userID => 123 }.to_json
+  get '/' do
+    "ScalaOne Sinatra app"
+  end
+
+  get '/messages' do
+    content_type :json
+    { :message => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', :userID => 123 }.to_json
+  end
+
+  post '/messages' do
+    Pusher['test_channel'].trigger('new_message', :message => params['message'])
+  end
 end
