409a25e2bcefce72bd335e413786e9cd92022513
Jean-Pierre Simard <jp@jpsim.com>
2012-08-23 12:22:33 -0400

    Added JSON endpoint

--- scalaone.rb
+++ scalaone.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
-  "Hello World!"
+  "ScalaOne Sinatra app"
+end
+
+get '/messages.json' do
+  content_type :json
+  { :key1 => 'value1', :key2 => 'value2' }.to_json
 end
